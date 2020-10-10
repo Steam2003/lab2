6d94d7679d6830b90152607c5d879f3dce8d1d93
Ivan Patiuk <gendalf2003@gmail.com>
2020-10-10 13:56:41 +0300

    lab2.2_step1

--- lab2.cpp
+++ lab2.cpp
@@ -14,16 +14,16 @@ int main()
 
 	double b; // вхідний параметр
 	double z1; // результат обчислення 1-го виразу
-	double z2; // результат обчислення 2-го виразу
+	//double z2; // результат обчислення 2-го виразу
 	cout << "b = "; cin >> b;
 
 	z1 = sqrt(2 * b + 2 * sqrt(b * b - 4)) / (sqrt(b * b - 4) + b + 2);
-	z2 = 1. / sqrt(b + 2);
+	//z2 = 1. / sqrt(b + 2);
 
 	cout << endl;
 
 	cout << "z1 = " << z1 << endl;
-	cout << "z2 = " << z2 << endl;
+	//cout << "z2 = " << z2 << endl;
 
 	cin.get();
 	return 0;
